5ad1165d0300dae436a8d18b6568f03965abef7f
Keane Quibilan <kquibilan@tribalscale.com>
2016-09-12 21:51:49 -0400

    KQ - Add first test.

---
+++ test/response_test.js
@@ -0,0 +1,22 @@
+"use strict";
+var assert = require('assert');
+
+describe('Response', function() {
+    describe('#constructor()', function() {
+        before(function() {
+
+        });
+
+        beforeEach(function() {
+
+        });
+
+        it('should return -1 when the value is not present', function() {
+            assert.equal(-1, [1,2,3].indexOf(4));
+        });
+
+        after(function() {
+
+        });
+    });
+});
