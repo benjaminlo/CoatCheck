0b3bc8e5a296496e95a496027c6c5115ab9dd523
Keane Quibilan <kquibilan@tribalscale.com>
2016-09-15 22:15:53 -0400

    KQ - Update to work on Node Runtime 4.3.

--- index.js
+++ index.js
@@ -6,52 +6,61 @@ const Speech = require('./alexa/speech.js');
 
 const APP_ID = ''; // replace with "amzn1.echo-sdk.amz-.app.[your-unique-value-here]"
 
-let onLaunch = (response, event, context, session) => {
+let onLaunch = (event, response) => {
     // Put your custom launch logic in here
     response.ask(new Speech(Constants.SPEECH_TYPE_SSML, '<speak>Replace this with a launch menu.</speak>'),
         new Speech(Constants.SPEECH_TYPE_SSML, '<speak>Replace this with reprompt text.</speak>'));
 };
 
-let onSessionEnded = (event, context, session) => {
+let onSessionEnded = (event) => {
     // Put your custom session ended logic in here
+    // session ended handler is unable to send a response
 };
 
 let intentHandlers = {};
-intentHandlers[Constants.YOUR_INTENT_NAME] = (response, event, context, session) => {
+intentHandlers[Constants.YOUR_INTENT_NAME] = (event, response) => {
+    // do stuff
     response.tell(new Speech(Constants.SPEECH_TYPE_SSML, '<speak>Replace this with your intent response.</speak>'));
 };
 
 // Remove this if you are not using Audio Player
 let audioPlayerHandlers = {};
-audioPlayerHandlers[Constants.REQUEST_TYPE_AUDIO_PLAYER_PLAYBACK_STARTED] = (event, context, session) => {
+audioPlayerHandlers[Constants.REQUEST_TYPE_AUDIO_PLAYER_PLAYBACK_STARTED] = (event, response) => {
     // do stuff
 };
-audioPlayerHandlers[Constants.REQUEST_TYPE_AUDIO_PLAYER_PLAYBACK_FAILED] = (event, context, session) => {
+audioPlayerHandlers[Constants.REQUEST_TYPE_AUDIO_PLAYER_PLAYBACK_FINISHED] = (event, response) => {
     // do stuff
 };
-audioPlayerHandlers[Constants.REQUEST_TYPE_AUDIO_PLAYER_PLAYBACK_STOPPED] = (event, context, session) => {
+audioPlayerHandlers[Constants.REQUEST_TYPE_AUDIO_PLAYER_PLAYBACK_STOPPED] = (event) => {
+    // do stuff
+    // playback stopped request is unable to send a response
+};
+audioPlayerHandlers[Constants.REQUEST_TYPE_AUDIO_PLAYER_PLAYBACK_NEARLY_FINISHED] = (event, response) => {
     // do stuff
 };
-audioPlayerHandlers[Constants.REQUEST_TYPE_AUDIO_PLAYER_PLAYBACK_NEARLY_FINISHED] = (event, context, session) => {
-    // do stuff
-};
-audioPlayerHandlers[Constants.REQUEST_TYPE_AUDIO_PLAYER_PLAYBACK_FAILED] = (event, context, session) => {
+audioPlayerHandlers[Constants.REQUEST_TYPE_AUDIO_PLAYER_PLAYBACK_FAILED] = (event, response) => {
     // do stuff
 };
 
 // Remove this if you are not using Playback Controller
 let playbackControllerHandlers = {};
-playbackControllerHandlers[Constants.REQUEST_TYPE_PLAYBACK_CONTROLLER_NEXT_COMMAND_ISSUED] = (event, directive) => {
+playbackControllerHandlers[Constants.REQUEST_TYPE_PLAYBACK_CONTROLLER_NEXT_COMMAND_ISSUED] = (event, response) => {
     // do stuff
 };
-playbackControllerHandlers[Constants.REQUEST_TYPE_PLAYBACK_CONTROLLER_PAUSE_COMMAND_ISSUED] = (event, directive) => {
+playbackControllerHandlers[Constants.REQUEST_TYPE_PLAYBACK_CONTROLLER_PAUSE_COMMAND_ISSUED] = (event, response) => {
     // do stuff
 };
-playbackControllerHandlers[Constants.REQUEST_TYPE_PLAYBACK_CONTROLLER_PLAY_COMMAND_ISSUED] = (event, directive) => {
+playbackControllerHandlers[Constants.REQUEST_TYPE_PLAYBACK_CONTROLLER_PLAY_COMMAND_ISSUED] = (event, response) => {
     // do stuff
 };
-playbackControllerHandlers[Constants.REQUEST_TYPE_PLAYBACK_CONTROLLER_PREVIOUS_COMMAND_ISSUED] = (event, directive) => {
+playbackControllerHandlers[Constants.REQUEST_TYPE_PLAYBACK_CONTROLLER_PREVIOUS_COMMAND_ISSUED] = (event, response) => {
     // do stuff
+};
+
+// Remove this if you do not want to add logic for System Exceptions
+let exceptionHandler = (event) => {
+    // do stuff
+    // exception handler is unable to send a response
 };
 
 let alexa = new Alexa(APP_ID)
@@ -60,4 +69,5 @@ let alexa = new Alexa(APP_ID)
     .setSessionEndedHandler(onSessionEnded)
     .setAudioPlayerHandlers(audioPlayerHandlers) // Remove this if you are not using Audio Player
     .setPlaybackControllerHandlers(playbackControllerHandlers) // Remove this if you are not using Playback Controller
+    .setSystemExceptionHandler(exceptionHandler) // Remove this if you do not want to add logic for System Exceptions
     .createLambdaConnection(exports); // This has to go last
